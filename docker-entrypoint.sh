c093fd2d3c574cfac427f70706026e841571a5ca
Sahidul Hassan <alrazon@gmail.com>
2021-09-26 14:26:52 +0600

    settings file

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /opt/app/app
+# Collect static files
+echo "Collecting static files"
+python manage.py collectstatic --noinput
+
+# Creating database migrations
+echo "Applying database migrations"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "Applying database migrations"
+python manage.py migrate
+
+# Start the server
+echo "Starting gunicorn"
+gunicorn --workers=3 \
+    --threads=4 \
+    --worker-class=gthread \
+    --chdir=/opt/app/app \
+    -b :3323 \
+    --log-level=info \
+    app.wsgi:application
+
+cd -

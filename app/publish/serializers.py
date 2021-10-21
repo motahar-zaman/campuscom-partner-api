c21df398503f9e4eccf1ca251a9ac6bb88d81ff4
Sakib Hasan <sakibccr@gmail.com>
2021-10-21 14:30:30 +0600

    latest

--- app/publish/serializers.py
+++ app/publish/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-from shared_models.models import Product
+from rest_framework_mongoengine.serializers import DocumentSerializer, EmbeddedDocumentSerializer
+
+from shared_models.models import Course, Section, Product
+from models.course.section import Section as SectionModel
+from models.courseprovider.course_provider import CourseProvider as CourseProviderModel
+from models.course.course import Course as CourseModel
+
+from rest_framework_mongoengine.fields import ReferenceField
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -12,3 +19,40 @@ class ProductSerializer(serializers.ModelSerializer):
             'total_quantity', 'quantity_sold', 'available_quantity',
             'is_active', 'tax_code', 'fee', 'currency_code'
         )
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'course_provider',
+            'title',
+            'content_ready',
+            'slug',
+            'content_db_reference',
+            'course_image_uri',
+            'external_image_url'
+        )
+
+
+class CourseModelSerializer(DocumentSerializer):
+    provider = ReferenceField(CourseProviderModel)
+
+    class Meta:
+        model = CourseModel
+
+
+class SectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Section
+        fields = ('id', 'course', 'name', 'fee', 'seat_capacity', 'available_seat',
+                  'execution_mode', 'registration_deadline', 'content_db_reference', 'is_active')
+
+
+class CheckSectionModelValidationSerializer(EmbeddedDocumentSerializer):
+
+    class Meta:
+        model = SectionModel
--- app/publish/views.py
+++ app/publish/views.py
@@ -1,15 +1,38 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
-from shared_models.models import Product
+from django.shortcuts import get_object_or_404
+
+from shared_models.models import Course, Section, Product, CourseProvider
+from models.course.course import Course as CourseModel
+from models.course.section import Section as SectionModel
+
+from rest_framework import serializers
+from datetime import datetime
 
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_201_CREATED,
-    HTTP_204_NO_CONTENT
 )
 
-from publish.serializers import ProductSerializer
+from rest_framework.decorators import api_view, permission_classes
+
+from publish.serializers import (
+    ProductSerializer,
+    SectionSerializer,
+    CheckSectionModelValidationSerializer,
+    CourseSerializer,
+    CourseModelSerializer
+)
+
 from publish.permissions import HasStoreAPIKey
+
+from django_scopes import scope, scopes_disabled
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import mimetypes
+import uuid
+
+from decouple import config
 
 
 class PublishViewSet(viewsets.ModelViewSet):
@@ -63,3 +86,238 @@ class PublishViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+@permission_classes([HasStoreAPIKey])
+def publish(request):
+    # prepare the data for mongodb
+    request_data = request.data.copy()
+
+    if 'provider' in request_data:
+        course_provider = get_object_or_404(CourseProvider, pk=str(request_data['provider']))
+        request_data['provider'] = course_provider.content_db_reference
+        request_data['course_provider'] = course_provider.id
+
+    if 'level' in request_data:
+        if request_data['level'] == 'unspecified':
+            request_data['level'] = ''
+
+    if 'course_image_uri' in request_data and request_data['course_image_uri']:
+        del request_data['course_image_uri']
+        # S3_IMAGE_DIR = config('S3_IMAGE_DIR', '')
+        # request_data['course_image_uri'] = rename_file(request_data['course_image_uri'], request_data['course_image_uri'].name)
+        # request_data['image'] = {'original': S3_IMAGE_DIR + '/' + request_data['course_image_uri'].name}
+
+    # preparation for mongodb done
+
+    request_data['from_importer'] = False
+
+    # course save in mongodb
+    course_model_serializer = CourseModelSerializer(data=request_data)
+
+    # import ipdb; ipdb.set_trace()
+
+    with scopes_disabled():
+        course_model_serializer.is_valid(raise_exception=True)
+    course_model_serializer.save()
+
+    # course save in postgres
+    request_data['content_db_reference'] = course_model_serializer.data['id']
+    course_serializer = CourseSerializer(data=request_data)
+    with scopes_disabled():
+        course_serializer.is_valid(raise_exception=True)
+    course_serializer.save()
+
+    data = course_model_serializer.data
+
+    data['course_image_uri'] = course_serializer['course_image_uri']
+    data['content_ready'] = course_serializer['content_ready']
+    data['provider'] = {'id': course_serializer['course_provider']['id'], 'name': course_serializer['course_provider']['name']}
+    data['id'] = course_serializer['id']
+    return Response(data, status=HTTP_201_CREATED)
+
+# class SectionViewSet(
+#     viewsets.ModelViewSet, SectionMixin
+# ):
+#     model = Section
+#     serializer_class = SectionSerializer
+#     http_method_names = ['head', 'patch', 'update']
+
+#     def get_queryset(self):
+#         fields = self.request.GET.copy()
+#         try:
+#             fields.pop('limit')
+#             fields.pop('page')
+#         except KeyError:
+#             pass
+
+#         return self.get_scoped_queryset(fields=fields)
+
+#     def create(self, request, *args, **kwargs):
+#         '''
+#         Creates section in postgres first. Then appends the section data in mongodb course sections.
+#         In mongodb section is not a individual document rather it is embedded in course.
+#         '''
+#         # handle 404 response manually
+#         with scope(**self.get_user_scope()):
+#             course = get_object_or_404(Course, id=request.data['course'])
+#         course_model = self.get_mongo_obj_or_404(
+#             CourseModel, id=course.content_db_reference
+#         )
+
+#         request_data = request.data.copy()
+
+#         if int(request_data['available_seat']) > int(request_data['seat_capacity']):
+#             raise serializers.ValidationError(
+#                 {
+#                     'available_seat': 'Available seat can not be greater than seat capacity'
+#                 }
+#             )
+
+#         start_date = None
+#         end_date = None
+#         registration_deadline = None
+
+#         if 'start_date' in request_data:
+#             start_date = datetime.fromisoformat(request_data['start_date'])
+
+#         if 'end_date' in request_data:
+#             end_date = datetime.fromisoformat(request_data['end_date'])
+
+#         if 'registration_deadline' in request_data:
+#             registration_deadline = datetime.fromisoformat(
+#                 request_data['registration_deadline']
+#             )
+
+#         if start_date and end_date and start_date > end_date:
+#             raise serializers.ValidationError(
+#                 {'start_date': 'Start date must not be in the future from end date'}
+#             )
+
+#         if registration_deadline and end_date and registration_deadline > end_date:
+#             raise serializers.ValidationError(
+#                 {
+#                     'registration_deadline': 'Final Enrollment Date must not be after end date'
+#                 }
+#             )
+
+#         data = self.prepare_embedded_section(request.data.copy())
+
+#         serializer_mongo = CheckSectionModelValidationSerializer(data=data)
+#         with scopes_disabled():
+#             serializer_mongo.is_valid(raise_exception=True)
+
+#         request_data['content_db_reference'] = course.content_db_reference
+#         serializer = SectionSerializer(data=request_data, context={'request': request})
+#         with scopes_disabled():
+#             serializer.is_valid(raise_exception=True)
+#         # use serializer.save() which returns a created object and handle exception manually
+#         self.perform_create(serializer)
+
+#         # Add instructors in section
+#         data = self.add_instructors_in_section(data, request_data)
+
+#         course_model.sections.append(SectionModel(**data))
+#         course_model.save()
+#         data = self.get_section_detail(serializer.data['id'])
+#         return Response(self.object_decorator(data), status=HTTP_201_CREATED)
+
+#     def update(self, request, *args, **kwargs):
+#         section = self.get_object()
+#         course_model = self.get_mongo_obj_or_404(
+#             CourseModel, id=section.content_db_reference
+#         )
+#         section_previous_name = section.name
+
+#         request_data = request.data.copy()
+
+#         if int(request_data['available_seat']) > int(request_data['seat_capacity']):
+#             raise serializers.ValidationError(
+#                 {
+#                     'available_seat': 'Available seat can not be greater than seat capacity'
+#                 }
+#             )
+
+#         start_date = None
+#         end_date = None
+#         registration_deadline = None
+
+#         # these incoming dates have Z suffix which is for zulu or to denote UTC timezone. bellow is a hack to fix this.
+#         # probably a better way to be to use dateutil or arrow
+
+#         if 'start_date' in request_data:
+#             start_date = datetime.fromisoformat(
+#                 request_data['start_date'].replace('Z', '+00:00')
+#             )
+
+#         if 'end_date' in request_data:
+#             end_date = datetime.fromisoformat(
+#                 request_data['end_date'].replace('Z', '+00:00')
+#             )
+
+#         if 'registration_deadline' in request_data:
+#             registration_deadline = datetime.fromisoformat(
+#                 request_data['registration_deadline'].replace('Z', '+00:00')
+#             )
+
+#         if start_date and end_date and start_date > end_date:
+#             raise serializers.ValidationError(
+#                 {'start_date': 'Start date must not be in the future from end date'}
+#             )
+
+#         if registration_deadline and end_date and registration_deadline > end_date:
+#             raise serializers.ValidationError(
+#                 {
+#                     'registration_deadline': 'Final Enrollment Date must not be after end date'
+#                 }
+#             )
+
+#         data = self.prepare_embedded_section(request.data.copy())
+
+#         serializer_mongo = CheckSectionModelValidationSerializer(data=data)
+#         with scopes_disabled():
+#             serializer_mongo.is_valid(raise_exception=True)
+
+#         serializer = SectionSerializer(section, data=request_data, partial=True)
+#         # handle exceptions manually
+#         with scopes_disabled():
+#             serializer.is_valid(raise_exception=True)
+#         # use serializer.partial_update() or serializer.save() which returns a created object and handle exception manually
+#         self.perform_update(serializer)
+
+#         serializer_mongo = CheckSectionModelValidationSerializer(data=data)
+#         with scopes_disabled():
+#             serializer_mongo.is_valid(raise_exception=True)
+#         # handle exceptions here and return appropriate erros messages
+
+#         # Add instructors in section
+#         data = self.add_instructors_in_section(data, request_data)
+
+#         CourseModel.objects(
+#             id=course_model.id, sections__code=section_previous_name
+#         ).update_one(set__sections__S=SectionModel(**data))
+#         data = self.get_section_detail(serializer.data['id'])
+#         return Response(self.object_decorator(data))
+
+
+def rename_file(file_object, image_name):
+    def getsize(f):
+        f.seek(0)
+        f.read()
+        s = f.tell()
+        f.seek(0)
+        return s
+
+    image_name = image_name.strip()
+    content_type, charset = mimetypes.guess_type(image_name)
+    size = getsize(file_object)
+    new_file_name = str(uuid.uuid4()) + '.' + str(content_type).split('/')[-1]
+    return InMemoryUploadedFile(
+        file=file_object,
+        name=new_file_name,
+        field_name=None,
+        content_type=content_type,
+        charset=charset,
+        size=size
+    )

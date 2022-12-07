60a77201b8c3fe164e99cabbbdf23b5814303579
Zaman <zaman@sgcsoft.net>
2022-12-07 09:24:19 +0600

    [CM-67] payment log related works, key change

--- app/webhooks/views.py
+++ app/webhooks/views.py
@@ -49,7 +49,7 @@ def handle_enrollment_event(payload, cart, course_provider):
                                 'name': payment.store_payment_gateway.name
                             }
                         },
-                        'type': 'capture request of order ' + str(payment.cart.order_ref)
+                        'summary': 'capture request of order ' + str(payment.cart.order_ref)
                     }
                     save_to_mongo(data, 'payment_request_response')
 
@@ -65,7 +65,7 @@ def handle_enrollment_event(payload, cart, course_provider):
                             'cart_id': str(payment.cart.id),
                             'order_ref': payment.cart.order_ref,
                         },
-                        'type': 'capture response of order ' + str(payment.cart.order_ref)
+                        'summary': 'capture response of order ' + str(payment.cart.order_ref)
                     }
                     save_to_mongo(data, 'payment_request_response')
 
@@ -105,7 +105,7 @@ def handle_enrollment_event(payload, cart, course_provider):
                         'name': payment.store_payment_gateway.name
                     }
                 },
-                'type': 'voidTransaction request of order ' + str(payment.cart.order_ref)
+                'summary': 'voidTransaction request of order ' + str(payment.cart.order_ref)
             }
             save_to_mongo(data, 'payment_request_response')
             capture, response = payment_transaction(payment, payment.store_payment_gateway, 'voidTransaction')
@@ -119,7 +119,7 @@ def handle_enrollment_event(payload, cart, course_provider):
                     'cart_id': str(payment.cart.id),
                     'order_ref': payment.cart.order_ref,
                 },
-                'type': 'voidTransaction response of order ' + str(payment.cart.order_ref)
+                'summary': 'voidTransaction response of order ' + str(payment.cart.order_ref)
             }
             save_to_mongo(data, 'payment_request_response')
             if capture:
